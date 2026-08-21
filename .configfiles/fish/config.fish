7711087c0ef6792786e05b170879033fce32f7fb
AxolotylThatLovesDreaming <authaxolotyl@gmail.com>
2026-08-21 23:54:17 +0530

    changes to rofi-power-menu and swaylock + swayidle setup

--- .configfiles/fish/config.fish
+++ .configfiles/fish/config.fish
@@ -1,7 +1,11 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-function fish_greeting
-    fastfetch
-end
+# function fish_greeting
+#     fastfetch
+# end
+
+alias hx="helix"
+
+set -gx EDITOR helix
 
 starship init fish | source
